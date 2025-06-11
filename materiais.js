5dbc1a37e317dd99c7b7ea94490c24c666a1e6fe
pokedaimon <admin@pokedaimon.com.br>
2025-06-11 13:31:37 -0300

    Add files via upload

---
+++ materiais.js
@@ -0,0 +1,14 @@
+const materias = [
+  {
+    id: "dynamax",
+    titulo: "Origem do Dynamax",
+    video: "banner_pokedaemon_inicial.mp4",
+    link: "materia1.html"
+  },
+  {
+    id: "wishingstar",
+    titulo: "Wishing Stars e Arceus",
+    video: "materia2-capa.mp4",
+    link: "materia2.html"
+  }
+];
